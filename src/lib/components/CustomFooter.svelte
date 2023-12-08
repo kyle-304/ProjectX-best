56b9832b0cd8becaf139a6c309d87455d06b541c
kyle sting <kyleonchoke2001@gmai.com>
2023-12-08 16:43:14 +0300

    fixing the routes and adding the about page.

--- src/lib/components/CustomFooter.svelte
+++ src/lib/components/CustomFooter.svelte
@@ -6,9 +6,9 @@
     <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <a class=" flex title-font font-medium items-center md:justify-start justify-center" href="/">
             <!-- you can put the logo here -->
-            <span class="ml-3 text-lg"> ABIRIA</span>
+            <span class="ml-3 text-lg"> PROJECT X</span>
         </a>
-        <p class="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2023 ABIRIA Driving School —
+        <p class="text-sm text-gray-400 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-800 sm:py-2 sm:mt-0 mt-4">© 2023 PROJECT X Driving School —
             <a href="/" class="text-gray-500 ml-1" target="_blank" rel="noopener noreferrer">@socialmedia</a>
         </p>
         <!-- social media -->
